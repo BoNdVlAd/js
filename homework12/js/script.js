602bed87836985525354fb417ee47c67a0c748b1
Bond Vlad <wlad23072003@gmail.com>
2023-07-21 15:00:50 +0300

    HW_12_fix2.0

--- homework12/js/script.js
+++ homework12/js/script.js
@@ -10,31 +10,20 @@ const charactersImages = {
 }
 
 const song = new Audio(`./assets/musics/Star-Wars.mp3`)
-
-
-
-
-
-
-
 const BASE_URL = "https://swapi.dev/api/"
 
+const nextBtn = document.querySelector('.next__btn')
 const form = document.querySelector('.search')
 const input = document.querySelector('.search input')
-
 const info = document.querySelector('.info')
 const items = document.createElement('ul')
 items.classList.add('items')
 info.appendChild(items)
-
 const planetsItems = document.querySelector('.planets__items')
 let pageCount = 0
 
-
-
-function renderCharacters(hero){
+function renderCharacter(hero){
     const {name, birth_year, gender} = {...hero}
-    console.log(hero)
     const item = document.createElement('li')
     item.classList.add('item')
     items.appendChild(item)
@@ -44,7 +33,6 @@ function renderCharacters(hero){
     <p class="birth">Birth Year: ${birth_year}</p>
     <p class="gender">Gender: ${gender}</p>
     `
-
 }
 
 async function getCharacters(characters){
@@ -55,13 +43,12 @@ async function getCharacters(characters){
     }).then((data) => {
       return data
     })
-    renderCharacters(y)
+    renderCharacter(y)
   }
-  return result
 }
 
-
-async function getInfo(e){
+function getInfo(e){
+  e.preventDefault()
   if(confirm("Чи не будете ви проти прослухати культовий саундтрек?")){
     song.play()
   }
@@ -69,47 +56,37 @@ async function getInfo(e){
   if(typeof item !== undefined){
     items.innerHTML = ``
   }
-  e.preventDefault()
   let filmNumber = input.value
   const movieTitle = document.querySelector('.title__info')
   movieTitle.textContent = `FILM N: ${filmNumber}`
 
-  await fetch(`${BASE_URL}films/${filmNumber}/`)
+  fetch(`${BASE_URL}films/${filmNumber}/`)
   .then(response => {
     return response.json()
   }).then((data) => {
-    return getCharacters(data.characters)
+    getCharacters(data.characters)
   })
 }
 
-const nextBtn = document.querySelector('.next__btn')
-
 function renderPlanets(data){
-  console.log(data)
-  
+  planetsItems.innerHTML = ``
   data.forEach(element => {
     const planetItem = document.createElement('li')
     planetItem.classList.add('planet__item')
     planetItem.textContent = element.name
     planetsItems.appendChild(planetItem)
   });
-  
-
 }
 
 async function nextPage(){
-  planetsItems.innerHTML = ``
+  planetsItems.innerHTML = `<h4>LOADING...<h4>`
   pageCount++
   const planets = await fetch(`https://swapi.dev/api/planets/?page=${pageCount}`)
   .then((response) => response.json())
   renderPlanets(planets.results)
 }
 
-
 nextBtn.addEventListener('click', nextPage)
-
-
-
 form.addEventListener('submit', e => getInfo(e))
 
 nextPage()
